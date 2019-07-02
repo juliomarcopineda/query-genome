96cf415f9a9e79da0a5bbb058e6bf7203dc7de2c
juliomarcopineda <juliomarco.pineda@nih.gov>
2019-07-02 15:22:47 -0400

    fixed gradle

---
+++ genomePoints/build.gradle.kts
@@ -0,0 +1,24 @@
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+
+plugins {
+    application
+    id("com.github.johnrengelman.shadow")
+    `java-library`
+}
+
+dependencies {
+    compile("info.picocli:picocli:3.9.6")
+}
+
+tasks.getByName<Zip>("shadowDistZip").archiveClassifier.set("shadow")
+tasks.getByName<Tar>("shadowDistTar").archiveClassifier.set("shadow")
+
+tasks.withType<ShadowJar> {
+    mergeServiceFiles()
+    isZip64 = true
+}
+
+application {
+    mainClassName = "query.genome.App"
+}
+
